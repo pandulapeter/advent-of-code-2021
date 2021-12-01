c7f460530a488ac6066517fa8c19566d3843325c
Péter Pandula <pandulapeter@gmail.com>
2021-12-01 15:56:16 +0200

    Day 1, Part 1 solution.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,44 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    println("Day 1")
+    val inputLines = readInput("Day01")
+    println("Read ${inputLines.size} lines")
+    try {
+        val inputValues = inputLines.map { it.toInt() }
+        var increaseCount = 0
+        var tendency: Tendency
+        inputValues.forEachIndexed { index, value ->
+            tendency = determineTendency(
+                previousValue = if (index == 0) null else inputValues[index - 1],
+                currentValue = value
+            )
+            println("$value (${tendency.describe()})")
+            if (tendency == Tendency.INCREASE) {
+                increaseCount++
+            }
+        }
+        println("There were a total of $increaseCount incremental measurements.")
+    } catch (_: NumberFormatException) {
+        println("Invalid input.")
     }
+}
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+private fun determineTendency(previousValue: Int?, currentValue: Int) = when {
+    previousValue == null -> Tendency.NO_PREVIOUS_MEASUREMENT
+    previousValue == currentValue -> Tendency.NO_CHANGE
+    previousValue < currentValue -> Tendency.INCREASE
+    else -> Tendency.DECREASE
+}
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+private enum class Tendency {
+    NO_PREVIOUS_MEASUREMENT,
+    INCREASE,
+    NO_CHANGE,
+    DECREASE
+}
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+private fun Tendency.describe() = when (this) {
+    Tendency.NO_PREVIOUS_MEASUREMENT -> "N/A - no previous measurement"
+    Tendency.INCREASE -> "increased"
+    Tendency.NO_CHANGE -> "unchanged"
+    Tendency.DECREASE -> "decreased"
 }
