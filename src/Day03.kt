f4c4cd50830da13ba49ee68dddfd55f46d9f9939
Péter Pandula <pandulapeter@gmail.com>
2021-12-03 11:52:55 +0200

    Day 3, Part 2 solution.

--- src/Day03.kt
+++ src/Day03.kt
@@ -17,6 +17,34 @@ fun main() {
         println("Gamma rate: $binaryGammaRate ($gammaRate)")
         println("Epsilon rate: $binaryEpsilonRate ($epsilonRate)")
         println("Power consumption: ${gammaRate * epsilonRate}")
+        println("\nDay 03 - Part Two\n")
+        var potentialOxygenGeneratorRatings = inputLines.toList()
+        var index = 0
+        while (potentialOxygenGeneratorRatings.size != 1) {
+            (potentialOxygenGeneratorRatings.count { it[index] == '1' } >= potentialOxygenGeneratorRatings.count { it[index] == '0' }).let { isOneTheMostCommonBit ->
+                potentialOxygenGeneratorRatings = potentialOxygenGeneratorRatings.filter {
+                    it[index] == if (isOneTheMostCommonBit) '1' else '0'
+                }
+            }
+            index++
+        }
+        val binaryOxygenGeneratorRating = potentialOxygenGeneratorRatings.first()
+        val oxygenGeneratorRating = binaryOxygenGeneratorRating.toInt(2)
+        println("Oxygen generator rating: $binaryOxygenGeneratorRating ($oxygenGeneratorRating)")
+        var potentialCO2ScrubberRatings = inputLines.toList()
+        index = 0
+        while (potentialCO2ScrubberRatings.size != 1) {
+            (potentialCO2ScrubberRatings.count { it[index] == '1' } < potentialCO2ScrubberRatings.count { it[index] == '0' }).let { isOneTheMostCommonBit ->
+                potentialCO2ScrubberRatings = potentialCO2ScrubberRatings.filter {
+                    it[index] == if (isOneTheMostCommonBit) '1' else '0'
+                }
+            }
+            index++
+        }
+        val binaryCO2ScrubberRating = potentialCO2ScrubberRatings.first()
+        val cO2ScrubberRating = binaryCO2ScrubberRating.toInt(2)
+        println("CO2 scrubber rating: $binaryCO2ScrubberRating ($cO2ScrubberRating)")
+        println("Life support rating: ${oxygenGeneratorRating * cO2ScrubberRating}")
     } catch (_: Exception) {
         println("Invalid input.")
     }
