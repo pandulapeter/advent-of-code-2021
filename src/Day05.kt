8753a7346bfad263ffa9050023a7731c12a3bd93
Péter Pandula <pandulapeter@gmail.com>
2021-12-05 13:16:26 +0200

    Day 5, Part 2 solution.

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,3 +1,4 @@
+import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
@@ -6,26 +7,26 @@ fun main() {
     println("Read ${inputLines.size} lines")
     try {
         println("\nDay 05 - Part One\n")
-        val points = inputLines.map { it.toLine().points }.flatten()
-        val groupedPoints = points.groupingBy { it }.eachCount()
-        val solution = groupedPoints.keys.count { (groupedPoints[it] ?: 0) >= 2}
-        println("There are $solution points where at least 2 lines overlap.")
-
+        println("There are ${inputLines.solve(false)} points where at least 2 lines overlap.")
+        println("\nDay 05 - Part Two\n")
+        println("There are ${inputLines.solve(true)} points where at least 2 lines overlap.")
     } catch (_: Exception) {
         println("Invalid input.")
     }
 }
 
-data class Point(
+private data class Point(
     val x: Int,
     val y: Int
 )
 
-data class Line(
+private data class Line(
     val start: Point,
-    val end: Point
+    val end: Point,
+    val shouldConsiderDiagonals: Boolean
 ) {
     val points = (if (start.x == end.x) {
+        print("Vertical")
         (min(start.y, end.y)..max(start.y, end.y)).map { y ->
             Point(
                 x = start.x,
@@ -33,21 +34,43 @@ data class Line(
             )
         }
     } else if (start.y == end.y) {
+        print("Horizontal")
         (min(start.x, end.x)..max(start.x, end.x)).map { x ->
             Point(
                 x = x,
                 y = start.y
             )
         }
-    } else emptyList()).also {
-        println("New line: ${it}")
-    }
+    } else if (shouldConsiderDiagonals) {
+        if (abs(start.x - end.x) == abs(start.y - end.y)) {
+            print("Diagonal")
+            (0..max(abs(start.x - end.x), abs(start.y - end.y))).map { iteration ->
+                Point(
+                    x = if (start.x > end.x) start.x - iteration else start.x + iteration,
+                    y = if (start.y > end.y) start.y - iteration else start.y + iteration
+                )
+            }
+        } else {
+            print("Invalid")
+            emptyList()
+        }
+    } else {
+        print("Invalid")
+        emptyList()
+    }).also { println(" line from $start to $end: $it") }
 }
 
-private fun String.toLine() = split(" -> ").let { points ->
+private fun List<String>.solve(shouldConsiderDiagonals: Boolean) = map { it.toLine(shouldConsiderDiagonals).points }
+    .flatten()
+    .groupingBy { it }
+    .eachCount()
+    .let { groupedPoints -> groupedPoints.keys.count { (groupedPoints[it] ?: 0) >= 2 } }
+
+private fun String.toLine(shouldConsiderDiagonals: Boolean) = split(" -> ").let { points ->
     Line(
         start = points.first().toPoint(),
-        end = points.last().toPoint()
+        end = points.last().toPoint(),
+        shouldConsiderDiagonals = shouldConsiderDiagonals
     )
 }
 
