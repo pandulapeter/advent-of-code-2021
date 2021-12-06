2cdd52561a2f3da6d1c39f324c327f08c998dc5a
Péter Pandula <pandulapeter@gmail.com>
2021-12-06 08:50:27 +0200

    Day 6, Part 2 solution.

--- src/Day06.kt
+++ src/Day06.kt
@@ -3,45 +3,40 @@ fun main() {
     println("Read ${inputLines.size} lines")
     try {
         println("\nDay 06 - Part One\n")
-        val lanternfish = inputLines.first().split(",").map { Lanternfish(it.toInt()) }.toMutableList()
-        var day = 0
-        var index: Int
-        var maxIndex: Int
-        lanternfish.log(day)
-        while (day < DAY_COUNT) {
-            day++
-            maxIndex = lanternfish.size
-            index = 0
-            while (index < maxIndex) {
-                if (lanternfish[index].process()) {
-                    lanternfish.add(Lanternfish())
-                }
-                index++
-            }
-            lanternfish.log(day)
-        }
+        inputLines.simulate(18)
+        println("\nDay 06 - Part Two\n")
+        inputLines.simulate(256)
     } catch (_: Exception) {
         println("Invalid input.")
     }
 }
 
-private data class Lanternfish(var counter: Int = FIRST_CYCLE_MAXIMUM) {
-
-    fun process() = (counter-- == 0).also { shouldSpawnNewInstance ->
-        if (shouldSpawnNewInstance) {
-            counter = CYCLE_MAXIMUM
+private fun List<String>.simulate(maximumDayCount: Int) {
+    val lanternfish = first()
+        .split(",")
+        .groupingBy { it.toInt() }
+        .eachCount()
+        .mapValues { it.value.toLong() }
+        .toMutableMap()
+    lanternfish.log(0)
+    var parentCount: Long
+    for (day in 1..maximumDayCount) {
+        parentCount = lanternfish[0] ?: 0L
+        for (index in 0..NEW_CYCLE_LENGTH) {
+            lanternfish[index] = when (index) {
+                CYCLE_LENGTH -> parentCount + (lanternfish[index + 1] ?: 0L)
+                NEW_CYCLE_LENGTH -> parentCount
+                else -> lanternfish[index + 1] ?: 0L
+            }
         }
-    }
-
-    companion object {
-        private const val CYCLE_MAXIMUM = 6
-        private const val FIRST_CYCLE_MAXIMUM = 8
+        lanternfish.log(day)
     }
 }
 
-private fun List<Lanternfish>.log(day: Int) = println(
-    "${if (day == 0) "Initial state" else "Day $day"}: $size lanternfish"
+private fun Map<Int, Long>.log(day: Int) = println(
+    "${if (day == 0) "Initial state" else "Day $day"}: ${values.sum()} lanternfish"
 )
 
-private const val DAY_COUNT = 80
+private const val CYCLE_LENGTH = 6
+private const val NEW_CYCLE_LENGTH = 8
 
