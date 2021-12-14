8c3b0794ccc24220c13e0444cdf447a613fac9ac
Péter Pandula <pandulapeter@gmail.com>
2021-12-14 12:18:03 +0200

    Day 14, Part 2 solution.

--- src/Day14.kt
+++ src/Day14.kt
@@ -2,39 +2,47 @@ fun main() {
     val inputLines = readInput("Day14")
     println("Read ${inputLines.size} lines")
     try {
-        println("\n${C_GREEN}Day 14 - Part One$C_RESET\n")
         val initialPolymerTemplate = inputLines.first()
-        val pairInsertionRules = inputLines.drop(2).map { line ->
-            line.split(" -> ").let {
-                InsertionRule(
-                    template = it.first(),
-                    insertion = it.last()
-                )
-            }
+        val replacementMap: Map<String, Char> = inputLines.drop(2).associate { line ->
+            line.split(" -> ").let { it.first() to it.last().first() }
         }
-        var polymerTemplate = initialPolymerTemplate
-        println(polymerTemplate)
-        repeat(10) { iteration ->
-            polymerTemplate = polymerTemplate.process(pairInsertionRules)
-            println("${iteration + 1}: $polymerTemplate")
-        }
-        println()
-        val counter = polymerTemplate.groupingBy { it }.eachCount().values
-        val mostCommonElementCount = counter.maxOrNull() ?: 0
-        val leastCommonElementCount = counter.minOrNull() ?: 0
-        println("Solution: $C_YELLOW${mostCommonElementCount - leastCommonElementCount}$C_RESET.")
+        println("\n${C_GREEN}Day 14 - Part One$C_RESET\n")
+        initialPolymerTemplate.process(replacementMap, 10)
+        println("\n${C_GREEN}Day 14 - Part Two$C_RESET\n")
+        initialPolymerTemplate.process(replacementMap, 40)
     } catch (_: Exception) {
         println("Invalid input.")
     }
 }
 
-private data class InsertionRule(
-    val template: String,
-    val insertion: String
-)
+private fun String.process(replacementMap: Map<String, Char>, iterationCount: Int) {
+    buildMap<Char, Long> {
+        increment(first())
+        (0 until iterationCount)
+            .fold(
+                initial = windowed(2).groupingBy { it }.eachCount().mapValues { it.value.toLong() }
+            ) { current, _ ->
+                buildMap {
+                    current.forEach { (segment, count) ->
+                        replacementMap[segment].let { newCharacter ->
+                            increment("${segment.first()}$newCharacter", count)
+                            increment("$newCharacter${segment.last()}", count)
+                        }
+                    }
+                }
+            }
+            .forEach { (segment, count) -> increment(segment.last(), count) }
+    }.print(iterationCount)
+}
 
-private fun String.process(insertionRules: List<InsertionRule>) = windowed(2).map { pair ->
-    (insertionRules.firstOrNull { it.template == pair }?.let { matchingInsertionRule ->
-        "${pair.first()}${matchingInsertionRule.insertion}"
-    } ?: pair.first())
-}.joinToString("").plus(last())
+private fun <T> MutableMap<T, Long>.increment(key: T, value: Long = 1) {
+    this[key] = (this[key] ?: 0) + value
+}
+
+private fun Map<Char, Number>.print(iterationCount: Int) {
+    println("Character frequency for $C_BLUE$iterationCount$C_RESET iterations:\n")
+    keys.sorted().forEach { println("${it}: ${this[it]}") }
+    val mostCommonElementCount = values.maxOf { it.toLong() }
+    val leastCommonElementCount = values.minOf { it.toLong() }
+    println("\nThe solution is $mostCommonElementCount - $leastCommonElementCount = $C_YELLOW${mostCommonElementCount - leastCommonElementCount}$C_RESET.")
+}
